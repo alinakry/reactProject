2c4a361e6de73e540497f7b2125a510bea8cff0b
alinakry <alinakrivopalceva9@gmail.com>
2024-10-25 00:40:46 +0300

    main

--- vite.config.ts
+++ vite.config.ts
@@ -1,24 +1,7 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
-// Middleware to set the correct MIME type for .tsx files
-function mimeTypesMiddleware() {
-  return {
-    name: 'mime-types-middleware',
-    configureServer(server: { middlewares: { use: (arg0: (req: any, res: any, next: any) => void) => void; }; }) {
-      server.middlewares.use((req, res, next) => {
-        if (req.url.endsWith('.tsx')) {
-          res.setHeader('Content-Type', 'text/javascript');
-        }
-        next();
-      });
-    },
-  };
-}
-
+// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), mimeTypesMiddleware()],
-  build: {
-    outDir: 'dist',
-  },
+  plugins: [react()],
 });
